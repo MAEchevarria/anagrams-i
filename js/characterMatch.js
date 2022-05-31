45b4e7baddd1a9ebe191def97017254fc17315c3
Michael Echevarria <michael.echevarria@live.com>
2022-05-31 09:09:29 -0500

    updated code for js

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,8 @@
 exports.isCharacterMatch = function(string1, string2) {
+    let newStr1  = string1.toLowerCase().split("").sort().join("").trim();
+    let newStr2  = string2.toLowerCase().split("").sort().join("").trim();
+    return (newStr1 === newStr2) 
+}
+// Having issues with case; zach === attack. Worked when testing.
 
-};
+// Added trim() for Challange question to test true.
